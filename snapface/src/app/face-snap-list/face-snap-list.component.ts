52dfa6bf11758941eede64a5bd916c17fd9e1078
Corentin <corentincolnat@gmail.com>
2022-04-27 08:15:51 +0200

    Angular V2 création formulaire réactif

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject, takeUntil, tap } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapService } from '../services/face-snaps.service';
 
@@ -7,14 +8,25 @@ import { FaceSnapService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
-
+  private destroy$!: Subject<boolean>;
   constructor(private faceSnapsService: FaceSnapService) { } //Argument type service permet angular injecte ce service (injection dépendance) 
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+    
+  }
+
+  ngOnDestroy(){
+    this.destroy$.next(true);
   }
 
 }
